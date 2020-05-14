d943fb2e46852153865c40064ceb84dcfe9de907
JackShao <jackshao1981@gmail.com>
2020-05-14 10:32:50 +0800

    add client-side form validation

--- Front_End_Web_Developer/evaluate-news-nlp/src/client/js/formHandler.js
+++ Front_End_Web_Developer/evaluate-news-nlp/src/client/js/formHandler.js
@@ -3,13 +3,27 @@ function handleSubmit(event) {
 
     // check what text was put into the form field
     let formText = document.getElementById('name').value
-    Client.checkForName(formText)
+    let data = Client.checkForName(formText)
+    console.log(data)
+    if(Object.keys(data).length === 0){
+        return false
+    }
+    
 
     console.log("::: Form Submitted :::")
-    fetch('http://localhost:8081/test')
+    fetch('http://localhost:8081/test',{
+        method:'POST',
+        credentials:'same-origin',
+        headers:{
+            'Content-Type':'application/json'
+        },
+        body:JSON.stringify(data)
+    })
     .then(res => res.json())
     .then(function(res) {
-        document.getElementById('results').innerHTML = res.message
+        document.getElementById('results').innerHTML = res.text
+    }).catch((e)=>{
+        console.log(e)
     })
 }
 
--- Front_End_Web_Developer/evaluate-news-nlp/src/server/index.js
+++ Front_End_Web_Developer/evaluate-news-nlp/src/server/index.js
@@ -2,6 +2,7 @@ var path = require('path')
 const express = require('express')
 const mockAPIResponse = require('./mockAPI.js')
 const cors = require('cors')
+const bodyParser = require('body-parser')
 const dotenv = require('dotenv')
 const aylien = require('aylien_textapi')
 dotenv.config()
@@ -15,13 +16,15 @@ const app = express()
 
 
 app.use(express.static('dist'))
+app.use(bodyParser.urlencoded({extended:false}))
+app.use(bodyParser.json())
 app.use(cors())
 
 console.log(__dirname)
 
 app.get('/', function (req, res) {
-    // res.sendFile('dist/index.html')
-    res.sendFile(path.resolve('src/client/views/index.html'))
+    res.sendFile('dist/index.html')
+    //res.sendFile(path.resolve('src/client/views/index.html'))
 })
 
 // designates what port the app will listen to for incoming requests
@@ -29,13 +32,16 @@ app.listen(8081, function () {
     console.log('Example app listening on port 8081!')
 })
 
-app.get('/test', function (req, res) {
-    textapi.sentiment({
-        'text':'John is a very good football player!'
-    },(error,response)=>{
+app.post('/test', function (req, res) {
+    let data=req.body
+    console.log(req.body)
+    textapi.sentiment(data,(error,response)=>{
         if(error === null){
             console.log(response)
+            res.send(response)
+        }else{
+            console.log(error)
         }
     })
-    res.send(mockAPIResponse)
+    //res.send(mockAPIResponse)
 })
